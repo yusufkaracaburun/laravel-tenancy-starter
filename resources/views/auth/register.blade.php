e43b99146f876b702a3698c349cdffdf950b7d26
yusufkaracaburun <yusuf.karacaburun@grandvision.com>
2024-02-22 18:08:16 +0100

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,57 +3,47 @@
 
     <!-- Company Name -->
     <div>
-        <x-input-label for="company" :value="__('Company')" />
-        <x-text-input id="company" class="block mt-1 w-full" type="text" name="company" :value="old('company')" required
+        <label for="company" >{{__('Company')}}</label>
+        <input id="company" class="block mt-1 w-full" type="text" name="company" :value="old('company')" required
                       autofocus />
-        <x-input-error :messages="$errors->get('company')" class="mt-2" />
     </div>
 
     <!-- Domain -->
     <div class="mt-2">
         <div class="flex items-baseline">
-            <x-input-label for="domain" :value="__('Domain')" />
-            <x-text-input id="domain" class="block mt-1 w-full" type="text" name="domain" :value="old('domain')"
+            <label for="domain" >{{__('Domain')}}</label>
+            <input id="domain" class="block mt-1 w-full" type="text" name="domain" :value="old('domain')"
                           required />
-            <x-input-error :messages="$errors->get('domain')" class="mt-2" />
             .{{ config ('tenancy.central_domains')[0]}}
         </div>
     </div>
 
     <!-- Name -->
     <div class="mt-2">
-        <x-input-label for="name" :value="__('Name')" />
-        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
+        <label for="name" >{{__('Name')}}</label>
+        <input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
                       required />
-        <x-input-error :messages="$errors->get('name')" class="mt-2" />
     </div>
 
     <!-- Email Address -->
     <div class="mt-4">
-        <x-input-label for="email" :value="__('Email')" />
-        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+        <label for="email" >{{__('Email')}}</label>
+        <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
                       required />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
     </div>
 
     <!-- Password -->
     <div class="mt-4">
-        <x-input-label for="password" :value="__('Password')" />
-
-        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+        <label for="password" >{{__('Password')}}</label>
+        <input id="password" class="block mt-1 w-full" type="password" name="password" required
                       autocomplete="new-password" />
-
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
     </div>
 
     <!-- Confirm Password -->
     <div class="mt-4">
-        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+        <label for="password_confirmation" >{{__('Confirm Password')}}</label>
+        <input id="password_confirmation" class="block mt-1 w-full" type="password"
                       name="password_confirmation" required />
-
-        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
     </div>
 
     <div class="flex items-center justify-end mt-4">
@@ -62,8 +52,8 @@
             {{ __('Already registered?') }}
         </a>
 
-        <x-primary-button class="ml-4">
+        <button class="ml-4">
             {{ __('Register') }}
-        </x-primary-button>
+        </button>
     </div>
 </form>
